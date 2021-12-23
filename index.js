4a08cba265029024863cf35cf3d24ad3d8bb8479
Fred <higgins.fred.s@gmail.com>
2021-12-23 13:21:16 -0800

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Add your code here
+
+
+function submitData (name, email) {
+    const configurationObject = {
+        method: "POST",
+        headers: {
+            "Content-type": "application/json",
+            Accept: "application/json"
+        },
+        body: JSON.stringify({name, email})
+    }
+    return fetch('http://localhost:3000/users', configurationObject)
+    .then(res => res.json())
+    .then(data => addUser(data.id))
+    .catch(error => {
+        document.body.innerHTML = "Unauthorized Access"
+    })
+
+};
+
+
+function addUser(user) {
+    document.body.innerHTML = user;
+}
